b0b0fa366042a29db1be88e7b9fdef80964f9dfc
fopsycute <fopsycute18@gmail.com>
2025-06-26 18:46:41 +0100

    fix: update asset paths to use site URL for consistency

--- footer.php
+++ footer.php
@@ -144,18 +144,18 @@
    <script src="https://js.paystack.co/v1/inline.js"></script> 
    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/drift-zoom/Drift.min.js"></script>
-  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="<?php echo $siteurl;?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo $siteurl;?>assets/vendor/php-email-form/validate.js"></script>
+  <script src="<?php echo $siteurl;?>assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="<?php echo $siteurl;?>assets/vendor/aos/aos.js"></script>
+  <script src="<?php echo $siteurl;?>assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="<?php echo $siteurl;?>assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="<?php echo $siteurl;?>assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="<?php echo $siteurl;?>assets/vendor/drift-zoom/Drift.min.js"></script>
+  <script src="<?php echo $siteurl;?>assets/vendor/purecounter/purecounter_vanilla.js"></script>
 
   <!-- Main JS File -->
-  <script src="assets/js/main.js"></script>
+  <script src="<?php echo $siteurl;?>assets/js/main.js"></script>
 <script type="text/javascript">const paymentForm = document.getElementById('paymentForm');
 paymentForm.addEventListener("submit", payWithPaystack, false);
 function payWithPaystack(e) {
--- index.php
+++ index.php
@@ -176,7 +176,7 @@ while ($row = mysqli_fetch_assoc($result)) {
     
       <!-- Image Column -->
       <div class="col-md-5 mb-4 mb-md-0">
-        <img src="assets/img/Sign up-pana.png" alt="Join Marketplace" class="img-fluid register-prompt-img">
+        <img src="<?php echo $siteurl;?>assets/img/Sign up-pana.png" alt="Join Marketplace" class="img-fluid register-prompt-img">
       </div>
       <!-- Content Column -->
       <div class="col-md-7">
@@ -309,7 +309,7 @@ while ($row = mysqli_fetch_assoc($result)) {
 
       <!-- Image Column -->
       <div class="col-md-5 mb-4 mb-md-0">
-        <img src="assets/img/Marketing-amico.png" alt="Join Marketplace" class="img-fluid affiliate-prompt-img">
+        <img src="<?php echo $siteurl;?>assets/img/Marketing-amico.png" alt="Join Marketplace" class="img-fluid affiliate-prompt-img">
       </div>
       <!-- Content Column -->
       <div class="col-md-7">
