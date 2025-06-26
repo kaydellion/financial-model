38dcc06626c7c62cc110e2d6586062a14b75865f
fopsycute <fopsycute18@gmail.com>
2025-06-26 15:38:52 +0100

    change variable name

--- backend/connect.php
+++ backend/connect.php
@@ -11,7 +11,7 @@ $db_pass = "";
 $db_name = "financial_model";
 */
 $db_username = "projectr_financial_model"; 
-$db_pass = "Y34GgwK(]h82Yg"; 
+$db_pass = "Y34GgwK(]h82Yg";
 $db_name = "projectr_finance";
 $con = mysqli_connect ("$db_host","$db_username","$db_pass","$db_name");
 mysqli_query($con, "SET time_zone = '+01:00'"); // Africa/Lagos
