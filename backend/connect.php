539fda3e61417ee62789ff52e52d66957d4fd01a
fopsycute <fopsycute18@gmail.com>
2025-07-12 05:04:29 +0100

    variable changed

--- backend/connect.php
+++ backend/connect.php
@@ -3,15 +3,15 @@
 error_reporting(E_ALL); ini_set('display_errors', 1); ini_set('log_errors', 1);
 $db_host = "localhost"; 
 /*
-$db_username = "projectr_financial_model"; 
-$db_pass = "Y34GgwK(]h82Yg"; 
-$db_name = "projectr_finance";
-*/
-
-
 $db_username = "root"; 
 $db_pass = ""; 
 $db_name = "financial_model";
+*/
+$db_username = "projectr_financial_model"; 
+$db_pass = "Y34GgwK(]h82Yg"; 
+$db_name = "projectr_finance";
+
+
 $con = mysqli_connect ("$db_host","$db_username","$db_pass","$db_name");
 mysqli_query($con, "SET time_zone = '+01:00'"); // Africa/Lagos
 $siteprefix="fm_";
